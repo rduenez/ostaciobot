0226afa4d911906c5ede4535f26de118b6d5fd47
Ronald Duenez <rduenez@tiempodevelopment.com>
2017-07-29 18:50:09 -0500

    Phase 2 - Initialization, drawing the map and adding colors

--- python/ostacio.py
+++ python/ostacio.py
@@ -14,15 +14,23 @@ class Game(object):
 
     #Window Properties
     RESOLUTION = WIDTH + 100,HEIGHT
-    WINDOWS_TITLE = 'Ostaciobot v0.0'
+    WINDOWS_TITLE = 'Ostaciobot v0.1'
 
     #Colors
     WHITE = (255, 255, 255)
     BLACK = (0, 0, 0)
+    FIREBRICK = (170, 13, 3)
+    STEELBLUE = (70,130,180)
+    FORESTGREEN = (34, 139, 34)
+    GOLD = (255, 215, 0)
     BACKGROUND = WHITE
 
+    #Map dimentions
     COLS = 20;
     ROWS = 20;
+
+    #Slot values
+    CLEAR = 0;
 
 
     def __init__(self):
@@ -31,10 +39,14 @@ class Game(object):
         pygame.init()
         self.screen = pygame.display.set_mode(self.RESOLUTION)
         self.clock = pygame.time.Clock()
+        self.map = [[0 for x in range(self.COLS)] for y in range(self.ROWS)]
         self.init()
 
     def init(self):
         self.done = False
+        for i in range(0,self.ROWS):
+            for j in range(0,self.COLS):
+                self.map[i][j] = self.CLEAR
 
     def update(self):
         # Handle exit events
@@ -50,6 +62,22 @@ class Game(object):
 
     def draw(self):
         self.screen.fill(self.BACKGROUND)
+        slot_width = (self.WIDTH//self.COLS)
+        slot_height = (self.HEIGHT//self.ROWS)
+
+        #Paint the map
+
+        for i in range(0, self.ROWS):
+            for j in range (0,self.COLS):
+                position_x = j*slot_width
+                position_y = i*slot_height
+                rect = pygame.Rect((position_x,position_y), (slot_width,slot_height))
+
+                if (self.map[i][j]==self.CLEAR):
+                    pygame.draw.rect(self.screen, self.WHITE, rect)
+
+                pygame.draw.rect(self.screen, self.BLACK, rect, 1)
+
         pygame.display.flip()
 
     def wait(self):
